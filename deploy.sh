b811c5e6d9615c8e3c4d28c3dfa082a7a62cb314
Martin Laws <martinblaws@gmail.com>
2019-04-27 19:05:54 -0400

    Updated deploy script to use correct repo name

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:martinlaws/big-todos.git master:gh-pages
+git push -f git@github.com:martinlaws/dsm-todos.git master:gh-pages
 
 cd -
