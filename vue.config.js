a8d49c0fd5dfc4d4cfa909896faaf0c0ae9db4e2
Martin Laws <martinblaws@gmail.com>
2019-07-10 23:24:10 -0400

    Removed publicPath conditional

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const formattedDSMVariables = require('./src/assets/dsm-styles.json')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: '/',
   css: {
     loaderOptions: {
       less: {
